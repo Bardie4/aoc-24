b05fa1af8c799affec323fe53df99ea0cad44416
Bardie4 <alda@equinor.com>
2024-12-11 07:50:38 +0100

    day 4 gold

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,7 +1,3 @@
-use std::str::FromStr;
-
-use regex::Regex;
-
 fn read_data(path: &str) -> Vec<Vec<char>> {
     let contents = std::fs::read_to_string(path).unwrap();
     contents
@@ -59,33 +55,37 @@ fn calculate_silver(data: &Vec<Vec<char>>) -> u32 {
 
 fn calculate_gold(data: &Vec<Vec<char>>) -> i32 {
     let mut total_sum = 0;
-    let directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
 
     for i in 0..data.len() {
         for j in 0..data[i].len() {
             if data[i][j] == 'A' {
-                for &(dx, dy) in &directions {
-                    let ni1 = i as isize + dx;
-                    let nj1 = j as isize + dy;
-                    let ni2 = i as isize - dx;
-                    let nj2 = j as isize - dy;
+                // Try to grab the 4 corners
+                let ul = (i as isize + 1, j as isize + 1);
+                let ur = (i as isize + 1, j as isize - 1);
+                let dl = (i as isize - 1, j as isize + 1);
+                let dr = (i as isize - 1, j as isize - 1);
 
-                    if ni1 >= 0
-                        && nj1 >= 0
-                        && ni2 >= 0
-                        && nj2 >= 0
-                        && ni1 < data.len() as isize
-                        && nj1 < data[i].len() as isize
-                        && ni2 < data.len() as isize
-                        && nj2 < data[i].len() as isize
+                if (ul.0 >= 0 && ul.1 >= 0)
+                    && (ur.0 >= 0 && ur.1 >= 0)
+                    && (dr.0 >= 0 && dr.1 >= 0)
+                    && (dl.0 >= 0 && dl.1 >= 0)
+                    && (ul.0 < data.len() as isize && ul.1 < data[i].len() as isize)
+                    && (dr.0 < data.len() as isize && dr.1 < data[i].len() as isize)
+                    && (ur.0 < data.len() as isize && ur.1 < data[i].len() as isize)
+                    && (dl.0 < data.len() as isize && dl.1 < data[i].len() as isize)
+                {
+                    // Construct diagonal strings, diagonal1 and diagonal2
+                    let diagonal1 = data[ul.0 as usize][ul.1 as usize].to_string()
+                        + &data[i][j].to_string()
+                        + &data[dr.0 as usize][dr.1 as usize].to_string();
+                    let diagonal2 = data[dl.0 as usize][dl.1 as usize].to_string()
+                        + &data[i][j].to_string()
+                        + &data[ur.0 as usize][ur.1 as usize].to_string();
+
+                    if (diagonal1 == "MAS" || diagonal1 == "SAM")
+                        && (diagonal2 == "MAS" || diagonal2 == "SAM")
                     {
-                        let first = data[ni1 as usize][nj1 as usize];
-                        let second = data[ni2 as usize][nj2 as usize];
-                        if (first == 'M' && second == 'S') || (first == 'S' && second == 'M') {
-                            total_sum += 1;
-                        }
-
-                        let third 
+                        total_sum += 1;
                     }
                 }
             }
@@ -97,7 +97,6 @@ fn calculate_gold(data: &Vec<Vec<char>>) -> i32 {
 
 pub fn solve(path: &str) {
     let data = read_data(path);
-    println!("input: {:?}", data);
 
     let silver_sum = calculate_silver(&data);
     println!("Silver sum: {}", silver_sum);
